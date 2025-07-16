e1b9bde5059a83e770c3fcfe3e7ec0a959e7ede5
conuaz <lucas.olasz@tty2000.com.br>
2025-07-16 16:51:55 -0300

    Body completo de consulta

--- src/main/java/com/ltech/transaction_money/authorization/AuthorizationRequest.java
+++ src/main/java/com/ltech/transaction_money/authorization/AuthorizationRequest.java
@@ -1,16 +1,20 @@
 package com.ltech.transaction_money.authorization;
 
-public record AuthorizationRequest(  
-    String jsonrpc,
-    String method,
-    Params params,
-    int id
-    ) {
-        
+public record AuthorizationRequest(
+        String jsonrpc,
+        String method,
+        Params params,
+        int id) {
+
     public record Params(
-        String apiKey,
-        int n,
-        int min,
-        int max
-    ) {}
+            String apiKey,
+            int n,
+            int min,
+            int max) {
+    }
+
+    public AuthorizationRequest generateInterBody() {
+        Params params = new Params("64680eaa-5a9b-4f40-b250-82c89b78a2d2", 1, 0, 1);
+        return new AuthorizationRequest("2.0", "generateIntegers", params, 1);
+    }
 }
